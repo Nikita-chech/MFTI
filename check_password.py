8b28e564b32041b5f5e8d3f124e19dac0fd56efd
Nikita-chech <126007981+Nikita-chech@users.noreply.github.com>
2023-03-01 21:42:15 +0300

    Update check_password.py

--- check_password.py
+++ check_password.py
@@ -2,14 +2,24 @@ def check_password(password):
     c=0
     if len(password)>=8:
         c+=1
-    if len(set(password))>len(set(password.upper())):
-        c+=1
+    l2=[]
+    l1=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    for i in range(len(l1)):
+        l2.append(str(i).upper())
+    for i in range(len(l1)):
+        if l1[i] in password:
+            c+=1
+            break
+    for i in range(len(l1)):
+        if l2[i] in password:
+            c+=1
+            break
     a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     for i in range(10):
         if str(a[i]) in password:
             c+=1
             break
-    if c==3:
+    if c==4:
         return True
     else:
         return False
