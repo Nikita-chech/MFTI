bdcfe1e91dbc1f63f754d6655f9b0f707d6afc8c
Nikita <126007981+Nikita-chech@users.noreply.github.com>
2023-05-09 23:45:52 +0300

    Add files via upload

---
+++ encode_word.py
@@ -0,0 +1,17 @@
+def encode_word(s):
+    alphabet = {'a': 'Alpha', 'b': 'Bravo', 'c': 'Charlie', 'd': 'Delta', 'e': 'Echo', 'f': 'Foxtrot', 'g': 'Golf', 'h': 'Hotel', 'i': 'India', 'j': 'Juliett', 'k': 'Kilo', 'l': 'Lima', 'm': 'Mike', 'n': 'November', 'o': 'Oscar', 'p': 'Papa', 'q': 'Quebec', 'r': 'Romeo', 's': 'Sierra', 't': 'Tango', 'u': 'Uniform', 'v': 'Victor', 'w': 'Whiskey', 'x': 'Xray', 'y': 'Yankee', 'z': 'Zulu'}
+    s1, l = [], []
+
+    for i in s:
+        if not(i in '0123456789'):
+            s1.append(i.lower())
+
+    for i in s1:
+        l.append(alphabet[i])
+
+    print(' '.join(l))
+
+encode_word('He432llo')
+
+
+
